55e85d1fbf9eef2b97b11b9e850c865ec6084e11
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-20 06:50:51 +0000

    Setup the interface

---
+++ main.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+interface = Tk()
+interface.title("A3AJAGBE RANDOM COCKTAIL RECIPE")
+interface.config(padx=30, pady=30)
+
+# Keep the screen open until exited
+interface.mainloop()
