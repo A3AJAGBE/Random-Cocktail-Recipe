9bf59776ba5c79c8a15653a2a6b1249d580dd751
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-21 07:46:00 +0000

    Remove image

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from tkinter import *
 import requests
-import io
-from urllib.request import urlopen
-# installed pillow
-from PIL import Image, ImageTk
 
 
 def get_recipe():
@@ -12,19 +8,6 @@ def get_recipe():
     response = requests.get(COCKTAIL_ENDPOINT)
     response.raise_for_status()
     data = response.json()['drinks']
-
-    # # Get cocktail image
-    # cocktail_image = data[0]['strDrinkThumb']
-    # image_url = f"{cocktail_image}/preview"
-    #
-    # # Read the image from the url
-    # image = urlopen(image_url)
-    # # create an image file object
-    # image = io.BytesIO(image.read())
-    # # use PIL to open image formats like .jpg  .png  .gif  etc.
-    # image = Image.open(image)
-    # # convert to an image Tkinter can use
-    # canvas.itemconfig(cocktail_picture, image)
 
     # Get cocktail name
     cocktail_name = data[0]["strDrink"]
@@ -79,32 +62,24 @@ TEXT = ("Courier", 15, "italic")
 COLOR = "#cc6699"
 
 # Interface Layout
-# canvas = Canvas(width=200, height=200)
-# cocktail_picture = ImageTk.PhotoImage()
-# placeholder_picture = PhotoImage(file="placeholder_image.png")
-# canvas.create_image(100, 100, image=placeholder_picture)
-# canvas.grid(row=0, column=0, columnspan=2)
-
 
 name_label = Label(text="", fg=COLOR, font=FONT)
-name_label.grid(row=1, column=0, columnspan=2)
-
+name_label.grid(row=0, column=0, columnspan=2)
 
 status_label = Label(text="", font=SUB_FONT)
-status_label.grid(row=2, column=0, columnspan=2)
-
+status_label.grid(row=1, column=0, columnspan=2)
 
 canvas = Canvas(width=500, height=200, bg=COLOR)
 instruction = canvas.create_text(250, 100, text="", width=450, font=TEXT, fill="white")
-canvas.grid(row=3, column=0, columnspan=2)
+canvas.grid(row=2, column=0, columnspan=2)
 
 ingredients = Listbox(width=40, font=TEXT)
 ingredients.insert(END, "")
-ingredients.grid(row=4, column=0, columnspan=2, pady=20)
+ingredients.grid(row=3, column=0, columnspan=2, pady=20)
 
 button = Button(text="Next", fg=COLOR, width=20, command=get_recipe)
 button.config(highlightthickness=0, padx=5, pady=5, font=SUB_FONT)
-button.grid(row=5, column=0, columnspan=2)
+button.grid(row=4, column=0, columnspan=2)
 
 get_recipe()
 
