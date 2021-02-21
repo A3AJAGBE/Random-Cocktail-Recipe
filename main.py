7288e6b8aa05cc12a85d584d04f93e2bb7439aab
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-21 07:23:24 +0000

    Add get recipe function

--- main.py
+++ main.py
@@ -5,30 +5,68 @@ from urllib.request import urlopen
 # installed pillow
 from PIL import Image, ImageTk
 
-COCKTAIL_ENDPOINT = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-response = requests.get(COCKTAIL_ENDPOINT)
-response.raise_for_status()
-data = response.json()['drinks']
 
-# Get the list of ingredients
-ingredients_list = []
-for num in range(1, 16):
-    item = f"strIngredient{num}"
-    cocktail_ingredient = data[0][item]
-    if cocktail_ingredient is not None:
-        add = f"{num}. {cocktail_ingredient}"
-        ingredients_list.append(add)
+def get_recipe():
 
-# Get ingredients measurements
-measurement_list = []
-for num in range(1, 16):
-    measure = f"strMeasure{num}"
-    cocktail_measurement = data[0][measure]
-    if cocktail_measurement is not None:
-        measurement_list.append(cocktail_measurement)
+    COCKTAIL_ENDPOINT = "https://www.thecocktaildb.com/api/json/v1/1/random.php"
+    response = requests.get(COCKTAIL_ENDPOINT)
+    response.raise_for_status()
+    data = response.json()['drinks']
 
-# combine the two lists
-new_dict = {k: v for k, v in zip(ingredients_list, measurement_list)}
+    # # Get cocktail image
+    # cocktail_image = data[0]['strDrinkThumb']
+    # image_url = f"{cocktail_image}/preview"
+    #
+    # # Read the image from the url
+    # image = urlopen(image_url)
+    # # create an image file object
+    # image = io.BytesIO(image.read())
+    # # use PIL to open image formats like .jpg  .png  .gif  etc.
+    # image = Image.open(image)
+    # # convert to an image Tkinter can use
+    # canvas.itemconfig(cocktail_picture, image)
+
+    # Get cocktail name
+    cocktail_name = data[0]["strDrink"]
+    name_label.config(text=cocktail_name)
+
+    # Get cocktail status
+    cocktail_status = data[0]["strAlcoholic"]
+    status_label.config(text=f"{cocktail_status} Cocktail")
+
+    # Get cocktail instruction
+    cocktail_instruction = data[0]['strInstructions']
+    canvas.itemconfig(instruction, text=cocktail_instruction)
+
+    # Get the list of ingredients
+    ingredients_list = []
+    for num in range(1, 16):
+        item = f"strIngredient{num}"
+        cocktail_ingredient = data[0][item]
+        if cocktail_ingredient is not None:
+            add = f"{num}. {cocktail_ingredient}"
+            ingredients_list.append(add)
+
+    # Get ingredients measurements
+    measurement_list = []
+    for num in range(1, 16):
+        measure = f"strMeasure{num}"
+        cocktail_measurement = data[0][measure]
+        if cocktail_measurement is not None:
+            measurement_list.append(cocktail_measurement)
+
+    # combine the two lists
+    ingredients.delete(0, END)
+    ingredients.insert(1, "The Cocktail Recipe: ")
+    combine_dict = {k: v for k, v in zip(ingredients_list, measurement_list)}
+    num = 1
+    for info in combine_dict:
+        num += 1
+        m = combine_dict[info]
+        data = f"{info} / {m}"
+        print(data)
+        ingredients.insert(num, data)
+
 
 # UI setup
 interface = Tk()
@@ -41,48 +79,34 @@ TEXT = ("Courier", 15, "italic")
 COLOR = "#cc6699"
 
 # Interface Layout
-cocktail_image = data[0]['strDrinkThumb']
-canvas = Canvas(width=200, height=200)
-image_url = f"{cocktail_image}/preview"
+# canvas = Canvas(width=200, height=200)
+# cocktail_picture = ImageTk.PhotoImage()
+# placeholder_picture = PhotoImage(file="placeholder_image.png")
+# canvas.create_image(100, 100, image=placeholder_picture)
+# canvas.grid(row=0, column=0, columnspan=2)
 
-# Read the image from the url
-image = urlopen(image_url)
-# create an image file object
-image = io.BytesIO(image.read())
-# use PIL to open image formats like .jpg  .png  .gif  etc.
-image = Image.open(image)
-# convert to an image Tkinter can use
-cocktail_picture = ImageTk.PhotoImage(image)
 
-canvas.create_image(100, 100, image=cocktail_picture)
-canvas.grid(row=0, column=0, columnspan=2)
-
-cocktail_name = data[0]["strDrink"]
-name_label = Label(text=cocktail_name, fg=COLOR, font=FONT)
+name_label = Label(text="", fg=COLOR, font=FONT)
 name_label.grid(row=1, column=0, columnspan=2)
 
-cocktail_status = data[0]["strAlcoholic"]
-status_label = Label(text=f"{cocktail_status} Cocktail", font=SUB_FONT)
+
+status_label = Label(text="", font=SUB_FONT)
 status_label.grid(row=2, column=0, columnspan=2)
 
-cocktail_instruction = data[0]['strInstructions']
+
 canvas = Canvas(width=500, height=200, bg=COLOR)
-canvas.create_text(250, 100, text=cocktail_instruction, width=450, font=TEXT, fill="white")
+instruction = canvas.create_text(250, 100, text="", width=450, font=TEXT, fill="white")
 canvas.grid(row=3, column=0, columnspan=2)
 
 ingredients = Listbox(width=40, font=TEXT)
-ingredients.insert(1, "The Cocktail Recipe: ")
-num = 1
-for data in new_dict:
-    num += 1
-    m = new_dict[data]
-    result = f"{data} / {m}"
-    ingredients.insert(num, result)
+ingredients.insert(END, "")
 ingredients.grid(row=4, column=0, columnspan=2, pady=20)
 
-button = Button(text="Next", fg=COLOR, width=20)
+button = Button(text="Next", fg=COLOR, width=20, command=get_recipe)
 button.config(highlightthickness=0, padx=5, pady=5, font=SUB_FONT)
 button.grid(row=5, column=0, columnspan=2)
+
+get_recipe()
 
 # Keep the screen open until exited
 interface.mainloop()
